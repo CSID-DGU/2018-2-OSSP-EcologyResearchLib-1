454ec443eb537c0ad87d30377c8a7acb6f7b4dc0
byeongseok <byeongseok@DESKTOP-KSTC9BL>
2018-11-19 17:00:15 +0900

    18/11/19 학명 추가

--- OSS/OSS/domain_eukaryotes.hpp
+++ OSS/OSS/domain_eukaryotes.hpp
@@ -21,13 +21,17 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. */
 
 /*
-Coded by : Byung Suk Oh
+Coded by : Byeong Seok Oh
 Last Updated : 18-11-05
 
 분류 : 역
 학명 : 진핵생물역 (Eukaryotes)
-설명 : https://namu.wiki/w/%EC%A7%84%ED%95%B5%EC%83%9D%EB%AC%BC
+설명 : 진핵세포를 가진 생물
+       진핵세포는 세포 내에 핵으로 대표되는 다양한 세포소기관을 가진 세포들을 통칭하는 이름이다.
+	   원핵세포의 DNA와는 달리, 진핵세포의 DNA에는 필요 없는 부분(인트론)이 대단히 많이 존재하며, 따라서 필요한 부분(엑손)을 이어서 mRNA를 만들게 된다. 
+	   진핵세포의 리보솜은 원핵세포보다 큰 80S이다.
 
+출처 : https://ko.wikipedia.org/wiki/진핵생물
 */
 
 #pragma once
