0b43d44b3ed785b45790404d6b3e7365b94b8530
사민 홍 <ghdtkals15@naver.com>
2018-12-02 21:42:49 +0900

    2018.12.02. 공동작업(알고리즘)

--- OSS/OSS/al_humpback_whale_mp.hpp
+++ OSS/OSS/al_humpback_whale_mp.hpp
@@ -41,6 +41,14 @@ SOFTWARE. */
 #include "al_algorithm.hpp"
 #include <vector>
 
+/* 9방향 이동 경로 : 정지, 북, 북동, 동, ... 서, 북서
+enum Direction
+{
+    STOP,
+    NORTH, NORTH_EAST, EAST, SOUTH_EAST,
+    SOUTH, SOUTH_WEST, WEST, NOTRH_WEST
+};
+*/
 
 // 혹등고래(Humpback Whale) 이동경로 예측(MP) 알고리즘
 class HumpbackWhaleMP : public MovementPrediction
@@ -54,35 +62,65 @@ protected:
 private:
     // 랜덤워커 배열 = 예측된 이동 경로
 	std::vector<RandomWalk> m_randomWalk; 
+    // 현재 랜덤워커 수
+    int m_number;
+    // 계산할 때마다 실시간으로 인접 9개의 필드 정보를 받아옴
+    LocalInfo* m_localInfo;
 
 public:
     virtual void initiate() override;
     virtual void predict() override;
 
-    // 9방향의 확률 계산 메소드
-    void calculate();
+    // 9방향의 확률 계산 메소드 : 랜덤워커 계산할 2차원 필드 범위를 지정
+    void calculate(int leftX, int topY, int rightX, int bottomY);
+    // 랜덤워커 객체 1개 추가
+    void addWalker();
+    // 특정 선호 요소에 따른 각 방향의 이동확률 계산
+    void calculateByPreference();
 };
 
 void HumpbackWhaleMP::initiate()
 {
     m_randomWalk.push_back(RandomWalk());   // 랜덤워커 배열의 0번 객체 초기화
 
-    //To Do
+    m_number = 1;
 }
 
 void HumpbackWhaleMP::predict()
 {
     // 한 번 수행될 때마다, 랜덤 워커 객체를 하나 생성하여 벡터에 추가한다.
+    // Prototype : 단위는 3일(3일마다 1위도(110km정도)씩 이동)
 
-    //To Do
+    addWalker();
 }
 
-void HumpbackWhaleMP::calculate()
+void HumpbackWhaleMP::calculate(int leftX, int topY, int rightX, int bottomY)
 {
     // 9방향 각각에 대한 이동 확률을 계산한다.
     // predict() 내부에서 실행된다.
 
-    //To Do
+    // ★ 계산할 때마다, 필드 정보의 업데이트가 필요하다.
+
+
+    // 각 선호 요소마다 계산 진행
+
+    // 개체 상태
+    calculateByPreference(/* state */);
+
+    // 수온
+    calculateByPreference(/* temperature */);
+
+    // LAST. 주변 지형 이동 가능성 판단 (이동 불가능 : 0 초기화)
+    calculateByPreference(/* 지형 */);
+}
+
+void HumpbackWhaleMP::addWalker()
+{
+    // 랜덤워커 벡터에 객체 하나 추가
+    m_randomWalk.push_back(RandomWalk());
+
+    // 랜덤워커 개체 수 카운트 증가
+    m_number++;
 }
 
 #endif // _HUMPBACK_WHALE_MOVEMENT_PREDICTION_HPP__
